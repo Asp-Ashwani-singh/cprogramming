ed96ee44b744599b50586d628979db5eb40817b4
Ashwani singh <rrvapmr@gmail.com>
2024-03-12 23:17:45 +0530

    Assignment2 and Assignment3 complete

--- assignment2.c
+++ assignment2.c
@@ -3,3 +3,49 @@
 //3. Write a program to input three characters from the user and display characters with their corresponding ASCII codes.
 //4. Write a program to print last digit of a given number.
 //5. Write a program to print a given number without its last digit.
+#include <stdio.h>
+
+/*
+1. Write a program to input a character from the user and print its ASCII code.
+int main(){
+    char a;
+    printf("Enter Character: ");
+    scanf("%c",&a);
+    printf("%d",a);
+    return 0;
+}
+
+2. Write a program to input an ASCII code from the user and print its corresponding character.
+int main(){
+    int a;
+    printf("Enter ASCII Code: ");
+    scanf("%d",&a);
+    printf("%c",a);
+    return 0;
+}
+
+3. Write a program to input three characters from the user and display characters with their corresponding ASCII codes.
+int main(){
+    char a,b,c;
+    printf("Enter Threee Character: ");
+    scanf("%c%c%c",&a,&b,&c);
+    printf("%c=%d %c=%d %c=%d",a,a,b,b,c,c);
+    return 0;
+}
+4. Write a program to print last digit of a given number.
+int main(){
+    int num;
+    printf("Enter The Number: ");
+    scanf("%d",&num);
+    printf("%d",num%10);
+    return 0;
+}
+5. Write a program to print a given number without its last digit.
+int main(){
+    int num,exaptlast;
+    printf("Enter The Number: ");
+    scanf("%d",&num);
+    printf("%d",num/10);
+    return 0;
+}
+*/
--- assignment3.c
+++ assignment3.c
@@ -3,3 +3,78 @@
 //3. Write a program to calculate simple interest.
 //4. Write a program to calculate volume of a cuboid.
 //5. Write a program to ask user about the cost price and selling price banana per dozen. Calculate the profit or loss earned upon selling 25 bananas.
+
+
+/*
+1. Write a program to calculate average of three integers. Numbers are given by the user.
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    double average;
+    printf("Enter Three Number: ");
+    scanf("%d%d%d",&a,&b,&c);
+    average=(a+b+c)/3;
+    printf("%lf",average);
+    return 0;
+}
+2. Write a program to calculate circumference of a circle.
+#include <stdio.h>
+int main(){
+    int r;
+    double circumference;
+    printf("Enter Radius of circle: ");
+    scanf("%d",&r);
+    circumference=2*3.14*r;
+    printf("%lf",circumference);
+    return 0;
+}
+3. Write a program to calculate simple interest.
+#include <stdio.h>
+int main(){
+    float p,r,d;
+    double sm;
+    printf("Enter principal amount ,rate and duration: ");
+    scanf("%f%f%f",&p,&r,&d);
+    sm=(p*r*d)/100;
+    printf("%lf",sm);
+    return 0;
+}
+4. Write a program to calculate volume of a cuboid.
+#include <stdio.h>
+int main(){
+    float length,width,height;
+    double volume;
+    printf("Enter length width height for cuboid: ");
+    scanf("%f%f%f",&length,&width,&height);
+    volume=length*width*height;
+    printf("%lf",volume);
+    return 0;
+}
+5. Write a program to ask user about the cost price and selling price banana per dozen. Calculate the profit or loss earned upon selling 25 bananas.
+#include <stdio.h>
+int main(){
+    int costPrice,salePrice;
+    double totalCost ,totalSale;
+    printf("Enter Cost Price: ");
+    scanf("%d",&costPrice);
+    printf("Enter Sale Price: ");
+    scanf("%d",&salePrice);
+    totalCost=costPrice*25;
+    totalSale=salePrice*25;
+    printf("Total profit or loss is %lf ",totalSale-totalCost);
+    return 0;
+}
+*/
+#include <stdio.h>
+int main(){
+    int costPrice,salePrice;
+    double totalCost ,totalSale;
+    printf("Enter Cost Price: ");
+    scanf("%d",&costPrice);
+    printf("Enter Sale Price: ");
+    scanf("%d",&salePrice);
+    totalCost=costPrice*25;
+    totalSale=salePrice*25;
+    printf("Total profit or loss is %lf ",totalSale-totalCost);
+    return 0;
+}
